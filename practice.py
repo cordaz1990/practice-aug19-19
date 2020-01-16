47b144eb589a23c4f15da2aa44e93da536096e1f
Cynthia Ordaz <cordaz@dons.usfca.edu>
2020-01-15 18:09:52 -0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,4 +1,20 @@
-print('hello')
-print('world')
-print('hello')
-print()
+def has_match(t1, t2):
+    for x, y in zip(t1, t2):
+        if x == y:
+          return True
+    return False
+  
+  
+#Dictionaries and Tuples 
+
+d = {'a':0, 'b':1, 'c':2}
+t = d.itmes()
+t 
+dict_items(['c', 2), ('a', 0), ('b', 1)}])
+
+for key, value in d.items():
+    print(key, value)
+    
+c 2
+a 0
+b 1
